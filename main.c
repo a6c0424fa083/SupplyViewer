d92bd3833e368d09318d28338b95c1cb78704262
Jannis Sauer <jannis_sauer@icloud.com>
2024-08-27 21:33:27 +0200

    basic 'hello world' project structure

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  SupplyViewer
+//
+//  Created by Jannis Sauer on 27.08.24.
+//
+
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+    printf("Hello, world!\n");
+    
+    
+    return 0;
+}
